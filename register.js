1667b82a4b6738343a711780e73b2369da854504
Jan <jan@nedralia.se>
2024-09-21 11:07:10 +0200

    feat(register): allow multiple entries in a single request

--- register.js
+++ register.js
@@ -1,10 +1,8 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event) => {
-    const body = JSON.parse(event.body);
-    const { clientId, duration, date, userId } = body;
-
+// Register a time entry and returns a promise
+function registerTimeEntry(clientId, duration, date, userId) {
     const params = {
         TableName: 'TimeEntries',
         Item: {
@@ -16,18 +14,43 @@ exports.handler = async (event) => {
         }
     };
 
+    return dynamo.put(params).promise();
+}
+
+exports.handler = async (event) => {
     try {
-        await dynamo.put(params).promise();
-        return {
+        const body = JSON.parse(event.body);
+
+        const { entries } = body;
+
+        if (!entries) {
+            return {
+                statusCode: 400,
+                message: 'Invalid request. Missing field `entries`.'
+            };
+        }
+
+        const promises = entries.map(entry => {
+            const { clientId, duration, date, userId } = entry;
+            return registerTimeEntry(clientId, duration, date, userId);
+        });
+
+        await Promise.all(promises);
+
+        const response = {
             statusCode: 200,
-            message: 'Time entry recorded.'
+            body: JSON.stringify({ message: 'Time entries recorded.' })
         };
+
+        return response;
     } catch (error) {
         console.error(error);
 
-        return {
+        const response = {
             statusCode: 500,
-            message: 'Could not record time entry.'
+            body: JSON.stringify({ message: 'Could not record time entry.' })
         };
+
+        return response;
     }
 };
