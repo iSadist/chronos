1fe74d167504e7c0cbc387ca71ad23c9cdbb4bc4
Jan <jan@nedralia.se>
2024-10-19 18:07:32 +0200

    ui: break out RegisterTimeView to own file

---
+++ chronos-frontend/src/components/RegisterTimeView.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react"
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { Button, NeutralButton } from "@/components/Button"
+import { ItemData } from "@/components/RowItem"
+
+import styles from "../app/page.module.css"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
+function RegisterTimeView({
+  items,
+  onRegister,
+}: { items: Array<ItemData>, onRegister: (hours: number, date: Date, project: string) => void }) {
+  const [hours, setHours] = useState(0)
+  const [project, setProject] = useState('')
+  const [date, setDate] = useState(new Date())
+
+  function onSubmit (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    onRegister(hours, date, project)
+  }
+
+  function handleQuickSelection (event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    const newValue = parseInt(event.currentTarget.textContent || '0')
+
+    // Set the hours to the value of the button
+    setHours(newValue)
+  }
+
+  function onChange (event: React.ChangeEvent<HTMLInputElement>) {
+    const newValue = parseInt(event.target.value)
+    setHours(newValue)
+  }
+
+  function onSetDate (newDate: Date) {
+    setDate(newDate)
+  }
+
+  function onSetProject (event: React.ChangeEvent<HTMLSelectElement>) {
+    setProject(event.target.value)
+  }
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+
+    setProject(items[0]?.name)
+  }, [items])
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <h2>Register time</h2>
+      <p>Register time on a project.</p>
+
+      <form className={styles.registerForm} onSubmit={onSubmit}>
+        <label>
+          Project / Client
+          <select className={styles.select} onChange={onSetProject}>
+            {items.map((item, index) => {
+              return <option key={index} value={item.name}>{item.name}</option>
+            })}
+          </select>
+        </label>
+        <div className={styles.inputRow}>
+          {/* TODO: Hide this initially and just register time for today. Also expose a button to change date if needed. */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar views={['day']} onChange={onSetDate} />
+          </LocalizationProvider>
+        </div>
+        <div className={styles.inputRow}>
+          <label>
+            Time spent
+          </label>
+          <input className={styles.field} type="number" onChange={onChange} value={hours} />
+          <NeutralButton action={handleQuickSelection} text={'1'} />
+          <NeutralButton action={handleQuickSelection} text={'2'} />
+          <NeutralButton action={handleQuickSelection} text={'4'} />
+          <NeutralButton action={handleQuickSelection} text={'8'} />
+        </div>
+        <Button text="Submit" action={() => {}} />
+      </form>
+    </ThemeProvider>
+  )
+}
+
+export default RegisterTimeView
