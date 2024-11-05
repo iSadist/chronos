1f46f60305ba198433927f679611a37256f2ae03
Jan <jan@nedralia.se>
2024-11-05 13:41:04 +0100

    ui: fix build

--- chronos-frontend/src/app/page.tsx
+++ chronos-frontend/src/app/page.tsx
@@ -4,13 +4,15 @@ import React from "react"
 import MainView from "@/components/MainView"
 
 export default function Home() {
-  // Check if there is a token and user id in local storage
-  const accessToken = localStorage.getItem("accessToken")
-  const userId = localStorage.getItem("userId")
-
-  if (!accessToken || !userId) {
-    // Navigate to login page if not logged in
-    window.location.href = "/login"
+  if (typeof window !== "undefined") {
+    // Check if there is a token and user id in local storage
+    const accessToken = localStorage.getItem("accessToken")
+    const userId = localStorage.getItem("userId")
+  
+    if (!accessToken || !userId) {
+      // Navigate to login page if not logged in
+      window.location.href = "/login"
+    }
   }
 
   return <MainView />
